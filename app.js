9afc7fb30d8815e01ff0bcd13c5caad7178e2b58
Aeiman-Fayyaz <aeimanfayyaz2002@gmail.com>
2025-04-07 01:06:27 +0500

    new code

---
+++ app.js
@@ -0,0 +1,12 @@
+const seconds = document.querySelector(".seconds")
+const minutes = document.querySelector(".minutes")
+const minute = document.querySelector(".minute")
+const hours = document.querySelector(".hours")
+
+for (let i = 0 ; i < 60 ; i++){
+    const mElement = document.createElement("i")
+    const sElement = document.createElement("i")
+    mElement.className = "element"
+    sElement.className = "element"
+    mElement.style = 
+}
